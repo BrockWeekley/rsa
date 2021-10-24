2ba3ec4ca04620ad3f780494ea0d5f1ed86a20f8
BrockWeekley <48456042+BrockWeekley@users.noreply.github.com>
2021-10-23 21:43:42 -0500

    Encryption and Decryption finished

--- rsa.cpp
+++ rsa.cpp
@@ -2,11 +2,9 @@
 #include <iostream>
 #include <string>
 #include <gmpxx.h>
-#include <filesystem>
-#include <unistd.h>
 using namespace std;
 
-#define K 2048
+#define K 10
 #define PADDING false
 
 void generatePrime(mpz_t &randomPrime, gmp_randstate_t state) {
@@ -35,17 +33,19 @@ void generatePrime(mpz_t &randomPrime, gmp_randstate_t state) {
 }
 
 void RSAkeygen() {
-    // Initialize p, q, n, and e
-    mpz_t intP, intQ, intN, intE;
-    mpz_init2(intP, K/2);
-    mpz_init2(intQ, K/2);
-    mpz_init2(intN, K);
+    // Initialize p, q, n, e, and d
+    mpz_t intP, intQ, intN, intE, intD;
+    mpz_init(intP);
+    mpz_init(intQ);
+    mpz_init(intN);
     mpz_init(intE);
+    mpz_init(intD);
     cout << "Generating p and q";
     int result = 0;
     // Generate p, q and n
     gmp_randstate_t state;
     gmp_randinit_default(state);
+    gmp_randseed_ui(state, time(NULL));
     while (result == 0) {
         generatePrime(intP, state);
         generatePrime(intQ, state);
@@ -62,51 +62,75 @@ void RSAkeygen() {
     mpz_mul(intN, intP, intQ);
     cout << "\n Determining Phi";
     // Initialize phi values
-    mpz_t phiN, phiP, phiQ, gcdNE, nMinusOne;
+    mpz_t phiN, phiP, phiQ, gcdNE;
     mpz_init(phiN);
     mpz_init(gcdNE);
-    mpz_init(nMinusOne);
-    mpz_init2(phiP, K/2);
-    mpz_init2(phiQ, K/2);
+    mpz_init(phiP);
+    mpz_init(phiQ);
     // Determine phi(n)
     mpz_sub_ui(phiP, intP, 1);
     mpz_sub_ui(phiQ, intQ, 1);
     mpz_mul(phiN, phiP, phiQ);
-    mpz_sub_ui(nMinusOne, phiN, 1);
     cout << "\n Generating e";
     // Generate e
-    mpz_random(intE, mpz_size(nMinusOne));
+    mpz_urandomm(intE, state, phiN);
     mpz_gcd(gcdNE,intE, phiN);
     while (mpz_cmp_ui(gcdNE, 1) != 0) {
-        mpz_random(intE, mpz_size(nMinusOne));
+        mpz_urandomm(intE, state, phiN);
         mpz_gcd(gcdNE,intE, phiN);
     }
-    cout << "\n Writing files";
-    char tmp[256];
-    getcwd(tmp, 256);
-    cout << "\n Working: " << tmp << endl;
+    // Generate d
+    mpz_invert(intD, intE, phiN);
     // Write values to files
-    FILE *pfile, *qfile, *efile;
-    pfile = fopen("program_files/p.txt", "ab+");
-    qfile = fopen("program_files/q.txt", "ab+");
-    efile = fopen("program_files/e.txt", "ab+");
-    size_t size = mpz_out_str(pfile, 10, intP);
-    printf("\n Size of pfile: %zu", size);
-    size = mpz_out_str(qfile, 10, intQ);
-    printf("\n Size of qfile: %zu", size);
-    size = mpz_out_str(efile, 10, intE);
-    printf("\n Size of efile: %zu", size);
+    FILE *pfile, *qfile, *efile, *dfile;
+    pfile = fopen("program_files/p.txt", "w");
+    qfile = fopen("program_files/q.txt", "w");
+    efile = fopen("program_files/e.txt", "w");
+    dfile = fopen("program_files/d.txt", "w");
+    mpz_out_str(pfile, 10, intP);
+    mpz_out_str(qfile, 10, intQ);
+    mpz_out_str(efile, 10, intE);
+    mpz_out_str(dfile, 10, intD);
     fclose(pfile);
     fclose(qfile);
     fclose(efile);
+    fclose(dfile);
 }
 
-void RSAencrypt() {
-    
+void RSAencrypt(mpz_t &message, mpz_t &e, mpz_t &n, char* location) {
+    mpz_t result;
+    mpz_init(result);
+    mpz_powm(result, message, e, n);
+//    char* resultString = new char[50000];
+//    char* messageString = new char[50000];
+//    printf("\n Encryption Result Val: ");
+//    mpz_get_str(resultString, 10, result);
+//    cout << resultString;
+//    printf("\n Message Val: ");
+//    mpz_get_str(messageString, 10, message);
+//    cout << messageString;
+    FILE* found = fopen(location, "w");
+    mpz_out_str(found, 10, result);
+    fclose(found);
 }
 
-void RSAdecrypt() {
+void RSAdecrypt(mpz_t &message, mpz_t &d, mpz_t &n, char* location) {
+    mpz_t result;
+    mpz_init(result);
+    mpz_powm(result, message, d, n);
     
+//    char* resultString = new char[50000];
+//    char* messageString = new char[50000];
+//    printf("\n Decryption Result Val: ");
+//    mpz_get_str(resultString, 10, result);
+//    cout << resultString << "\n";
+//    printf("\n Message Val: ");
+//    mpz_get_str(messageString, 10, message);
+//    cout << messageString << "\n";
+
+    FILE* found = fopen(location, "w");
+    mpz_out_str(found, 10, result);
+    fclose(found);
 }
 
 void RSAPaddingEncrypt() {
@@ -121,12 +145,14 @@ int main(int argc, char** argv ) {
     cout << "Generating new keys";
     RSAkeygen();
     
-    mpz_t p, q, n, e, d;
-    mpz_init2(p, K / 2);
-    mpz_init2(q, K / 2);
-    mpz_init2(n, K);
+    mpz_t p, q, n, e, d, message, endmessage;
+    mpz_init(p);
+    mpz_init(q);
+    mpz_init(n);
     mpz_init(e);
     mpz_init(d);
+    mpz_init(message);
+    mpz_init(endmessage);
     
     char *file_location = new char[100];
     cout << "\n Enter the name of the file that contains p: \n";
@@ -150,35 +176,59 @@ int main(int argc, char** argv ) {
 
     cout << "Enter the output file name to store d: \n";
     cin >> file_location;
-    FILE *dfile;
-    dfile = fopen(file_location, "ab+");
-    mpz_out_str(dfile, 10, d);
+    FILE *dfileout, *dfile;
+    dfile = fopen("program_files/d.txt", "r");
+    mpz_inp_str(d, dfile, 10);
+    dfileout = fopen(file_location, "w");
+    mpz_out_str(dfileout, 10, d);
     
     cout << "Enter the output file name to store N: \n";
     cin >> file_location;
     FILE *nfile;
-    nfile = fopen(file_location, "ab+");
+    nfile = fopen(file_location, "w");
+    mpz_mul(n, p, q);
+    mpz_out_str(nfile, 10, n);
     
     cout << "Enter the name of the file that contains x to be encrypted using (N, e): \n";
     cin >> file_location;
     FILE *xfile;
     xfile = fopen(file_location, "r");
-//
-//    cout << "Enter the output file name to store E(x): \n";
-//    string encryptionout;
-//    cin >> encryptionout;
-//
-//    cout << "Enter the name of the file that contains c to be decrypted using d: \n";
-//    string cfile;
-//    cin >> cfile;
-//
-//    cout << "Enter the output file name to store D(c): \n";
-//    string decryptionout;
-//    cin >> decryptionout;
+    mpz_inp_str(message, xfile, 10);
+    
+    cout << "Enter the output file name to store E(x): \n";
+    cin >> file_location;
+    char* encryption = new char[100];
+    encryption = file_location;
+    
+    if (PADDING) {
+        
+    } else {
+        RSAencrypt(message, e, n, encryption);
+    }
 
+    cout << "Enter the name of the file that contains c to be decrypted using d: \n";
+    cin >> file_location;
+    FILE *cfile;
+    cfile = fopen(file_location, "r");
+    mpz_inp_str(endmessage, cfile, 10);
+    
+    cout << "Enter the output file name to store D(c): \n";
+    cin >> file_location;
+    char* decryption = new char[100];
+    decryption = file_location;
+    
+    if (PADDING) {
+        
+    } else {
+        RSAdecrypt(endmessage, d, n, decryption);
+    }
+    
     fclose(pfile);
     fclose(qfile);
     fclose(efile);
     fclose(dfile);
+    fclose(dfileout);
     fclose(nfile);
+    fclose(cfile);
+    fclose(xfile);
 }
